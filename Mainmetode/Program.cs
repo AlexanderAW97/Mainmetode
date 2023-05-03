a5cdbf4098de35186b0943783132d5b40f279b2a
AlexanderAW97 <alexanderw@getacademy.no>
2023-05-03 13:35:30 +0200

    Add project files.

---
+++ Mainmetode/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Mainmetode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string ord in args) {
+                Console.WriteLine(ord);
+            }
+                Console.WriteLine(args.Length);
+
+            
+        }
+    }
+}
